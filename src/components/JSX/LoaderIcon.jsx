8b4f013f80756ffc26607348c39b57fbf7ad1359
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-06-06 00:25:59 +0530

    style: add spinning loader

--- src/components/JSX/LoaderIcon.jsx
+++ src/components/JSX/LoaderIcon.jsx
@@ -2,42 +2,16 @@ import React from "react";
 import "../CSS/LoaderIcon.css"
 const LoaderIcon = () => {
   return (
-    <div className="loader-wrapper">
-      <div className='loader-line' id='loader-line-one'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-two'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-three'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-four'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-five'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-six'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-seven'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-eight'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-nine'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-ten'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-eleven'>
-        <div className='stick'></div>
-      </div>
-      <div className='loader-line' id='loader-line-twelve'>
-        <div className='stick'></div>
+    <div class="loader-wrapper">
+      <div class = "loader-icon">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   )
