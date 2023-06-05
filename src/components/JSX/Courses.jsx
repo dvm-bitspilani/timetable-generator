486fd0b1c70f9754c071cc4e032667aa42eed387
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-06-06 04:35:09 +0530

    fix: moving from homepage to courses

--- src/components/JSX/Courses.jsx
+++ src/components/JSX/Courses.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import Async from "react-async"
 import Loader from "./Loader";
+import CourseList from "./CourseList";
 
 const onInputID = () =>{
-  return fetch("https://jsonplaceholder.typicode.com/users")
+  return fetch("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates.json")
   .then(res => (res.ok ? res : Promise.reject(res)))
   .then(res => res.json())
 };
 
 const Courses = () => {
-  // <Async promiseFn={onInputID}>
-  //   {({ data, err, isLoading }) => {
-  //     if (isLoading) return <Loader />;
-  //     if (err) return `Something went wrong: ${err.message}`;
+  return (
+    <Async promiseFn={onInputID}>
+      {({ data, error, isLoading }) => {
+        if (isLoading) return <Loader />;
+        if (error) return `Something went wrong: ${error.message}`;
 
-  //     if (data)
-  //       return (
-  //         <CourseList />
-  //       );
-  //   }}
-  // </Async>;
-  return(
-    <h1>test</h1>
-  )
+        if (data) return <CourseList data={data} />;
+        
+        return null;
+      }}
+    </Async>
+  );
 };
 
 export default Courses;
