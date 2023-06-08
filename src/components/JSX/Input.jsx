cea247330cf8e1732d174347bfa18abdb75b280e
Luv Gupta <lg5122003@gmail.com>
2023-06-08 10:47:44 +0530

    feat: add ID validation

--- src/components/JSX/Input.jsx
+++ src/components/JSX/Input.jsx
@@ -7,25 +7,40 @@ import submitbtn from "../../../src/assets/button-icon.svg";
 function Input({setShowInputBox}) {
 
   const [inputValue, setInputValue] = useState('');
+  const [showError, setShowError] = useState('');
 
   const handleInputChange = (event) => {
     const capitalizedValue = event.target.value.toUpperCase();
     setInputValue(capitalizedValue);
   };
 
-  const goToCourses = ()=>{
-    setShowInputBox(false);
+  const validateForm = (e) => {
+    e.preventDefault();
+
+    const idRegex = /^20[12][1237890]([BD][1-5])?[ABD][1-8AB][PT]S[0-9]{4}[PGH]$/;
+
+    if (!idRegex.test(inputValue)) {
+      setShowError('Enter a valid ID!');
+    }
+    else {
+      setShowError('');
+      setShowInputBox(false);
+    }
+    
   }
     
   return (
-    <div className="input-area">
-      <div className="logo">
-        <img src={dvmlogo} alt="" />
+    <div className="input-id">
+      <div className="input-area">
+        <div className="logo">
+          <img src={dvmlogo} alt="" />
+        </div>
+        <form onSubmit={validateForm}>
+          <input type="text" placeholder='Enter your ID Number' value={inputValue} onChange={handleInputChange} />
+          <button type="submit" className='btn-icon'><img src={submitbtn} alt="" /></button>
+        </form> 
       </div>
-      <form>
-        <input type="text" placeholder='Enter your ID Number' value={inputValue} onChange={handleInputChange} />
-        <button type="submit" className='btn-icon' onClick={goToCourses}><img src={submitbtn} alt="" /></button>
-      </form>
+      <span className="error-msg">{showError}</span>
     </div>
   )
 }
