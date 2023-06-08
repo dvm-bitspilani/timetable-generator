b99c79543ea8597d92a1d3e166b4b6f6fc78b19a
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-06-09 00:11:38 +0530

    style: buttons ui

--- src/components/JSX/PercentageBar.jsx
+++ src/components/JSX/PercentageBar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import IconBolt from "../../assets/IconBolt.svg";
+import IconPlus from "../../assets/IconPlus.svg";
+import "../CSS/PercentageBar.css";
 
-const PercentageBar = () =>{};
+const PercentageBar = () =>{
+  return(
+    <div className="course-buttons-container">
+      <div className="course-buttons">Generate Timetable <img src={IconBolt} alt="Lightning Bolt" /></div>
+      <div className="course-buttons">Add More Courses <img src={IconPlus} alt="Plus" /></div>
+    </div>
+  );
+};
 
 export default PercentageBar;
