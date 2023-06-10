31d81cd32b49d2b1a57cc2ed685c0ab43c302578
BharatRajSingal <126271100+GeekWolf007@users.noreply.github.com>
2023-06-10 05:49:45 +0530

    refractor

--- src/components/JSX/AddMoreCourse.jsx
+++ src/components/JSX/AddMoreCourse.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import "../CSS/AddMoreCourse.css";
+import IconSearch from "../../assets/IconSearch.svg"
 
 const AddMoreCourse = () =>{
   return(
-    <h1>add more course test</h1>
+    <div className="add-more-courses-container">
+      <div className="add-more-courses">
+        <div className="amc-searchbox"></div>
+        <div className="amc-courses-container"></div>
+        <div className="amc-add-btn"></div>
+      </div>
+    </div>
   )
 };
 
