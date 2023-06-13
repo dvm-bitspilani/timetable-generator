11a5d2131fc0fbd483be032d32725d1a60de840c
Luv Gupta <lg5122003@gmail.com>
2023-06-13 06:47:01 +0530

    feat: add want/don't want switch

--- src/components/JSX/Lectures.jsx
+++ src/components/JSX/Lectures.jsx
@@ -1,13 +1,36 @@
-import React ,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import "../CSS/Lectures.css";
 
-const Lectures = ({courseId}) =>{
-
+const Lectures = ({ courseId }) => {
   const lectureArray = [
-    {id:1, lecture: "L 1" , room: 5101 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
-    {id:2, lecture: "L 2" , room: 5102 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
-    {id:3, lecture: "L 3" , room: 5103 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
-    {id:4, lecture: "L 4" , room: 5104 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
+    {
+      id: 1,
+      lecture: "L 1",
+      room: 5101,
+      faculty: "Prof Shivang Rai",
+      hours: "0900Hrs - 0950Hrs",
+    },
+    {
+      id: 2,
+      lecture: "L 2",
+      room: 5102,
+      faculty: "Prof Shivang Rai",
+      hours: "0900Hrs - 0950Hrs",
+    },
+    {
+      id: 3,
+      lecture: "L 3",
+      room: 5103,
+      faculty: "Prof Shivang Rai",
+      hours: "0900Hrs - 0950Hrs",
+    },
+    {
+      id: 4,
+      lecture: "L 4",
+      room: 5104,
+      faculty: "Prof Shivang Rai",
+      hours: "0900Hrs - 0950Hrs",
+    },
     // {id:5, lecture: "L 4" , room: 5104 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
     // {id:6, lecture: "L 1" , room: 5101 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
     // {id:7, lecture: "L 2" , room: 5102 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"},
@@ -16,44 +39,94 @@ const Lectures = ({courseId}) =>{
     // {id:10, lecture: "L 4" , room: 5104 , faculty: "Prof Shivang Rai", hours: "0900Hrs - 0950Hrs"}
   ];
 
-  useEffect(() => {
-    const storedDivs = JSON.parse(localStorage.getItem('clickedDivs')) || [];
-    storedDivs.forEach(divId => {
-      const targetDiv = document.getElementById(divId);
-      if (targetDiv) {
-        targetDiv.classList.add('lecture-card-selected');
-      }
-    });
-  }, []);
+  const [want, setWant] = useState(true);
 
-  const onLectureClick = (e) =>{
+  useEffect(() => {
+    const allSections = document.querySelectorAll(".lecture-card");
+    allSections.forEach((section) => {
+      section.classList.remove("lecture-card-selected");
+    });
+    if (want == true) {
+      const wantedSections =
+        JSON.parse(localStorage.getItem("wantedSections")) || [];
+      wantedSections.forEach((divId) => {
+        const targetDiv = document.getElementById(divId);
+        if (targetDiv) {
+          targetDiv.classList.add("lecture-card-selected");
+        }
+      });
+    } else {
+      const unwantedSections =
+        JSON.parse(localStorage.getItem("unwantedSections")) || [];
+      unwantedSections.forEach((divId) => {
+        const targetDiv = document.getElementById(divId);
+        if (targetDiv) {
+          targetDiv.classList.add("lecture-card-selected");
+        }
+      });
+    }
+  }, [want]);
+
+  const onLectureClick = (e) => {
     console.log(e.currentTarget);
     console.log(e.target);
     const targetDiv = e.currentTarget;
     if (targetDiv.className === "lecture-card") {
       targetDiv.className = "lecture-card lecture-card-selected";
 
-      let storedDivs = JSON.parse(localStorage.getItem('clickedDivs')) || [];
-      storedDivs.push(targetDiv.id);
-      localStorage.setItem('clickedDivs', JSON.stringify(storedDivs));
-      
+      if (want == true) {
+        let wantedSections =
+          JSON.parse(localStorage.getItem("wantedSections")) || [];
+        wantedSections.push(targetDiv.id);
+        localStorage.setItem("wantedSections", JSON.stringify(wantedSections));
+      } else {
+        let unwantedSections =
+          JSON.parse(localStorage.getItem("unwantedSections")) || [];
+        unwantedSections.push(targetDiv.id);
+        localStorage.setItem(
+          "unwantedSections",
+          JSON.stringify(unwantedSections)
+        );
+      }
     } else if (targetDiv.className === "lecture-card lecture-card-selected") {
       targetDiv.className = "lecture-card";
 
-      let storedDivs = JSON.parse(localStorage.getItem('clickedDivs')) || [];
-      const index = storedDivs.indexOf(targetDiv.id);
-      if (index > -1) {
-        storedDivs.splice(index, 1);
-        localStorage.setItem('clickedDivs', JSON.stringify(storedDivs));
+      if (want == true) {
+        let wantedSections =
+          JSON.parse(localStorage.getItem("wantedSections")) || [];
+        const index = wantedSections.indexOf(targetDiv.id);
+        if (index > -1) {
+          wantedSections.splice(index, 1);
+          localStorage.setItem(
+            "wantedSections",
+            JSON.stringify(wantedSections)
+          );
+        }
+      } else {
+        let unwantedSections =
+          JSON.parse(localStorage.getItem("unwantedSections")) || [];
+        const index = unwantedSections.indexOf(targetDiv.id);
+        if (index > -1) {
+          unwantedSections.splice(index, 1);
+          localStorage.setItem(
+            "unwantedSections",
+            JSON.stringify(unwantedSections)
+          );
+        }
       }
     }
   };
 
-  return(
+  return (
     <div className="lectures" id={courseId}>
       <div className="lectures-container">
-        {lectureArray.map(item=>(
-          <div key={item.id} id={`lecture-card${item.id}${courseId.replace(/ +/g, "")}`} className="lecture-card" onClick={onLectureClick}>
+        {lectureArray.map((item) => (
+          <div
+            key={item.id}
+            id={`lecture-card${item.id}${courseId.replace(/ +/g, "")}`}
+            className="lecture-card"
+            onClick={onLectureClick}
+          >
             <div className="lecture-room">
               <h3 className="font-weight-600">{item.lecture}</h3>
               <h3 className="font-weight-500">{item.room}</h3>
@@ -64,10 +137,23 @@ const Lectures = ({courseId}) =>{
         ))}
       </div>
       <div className="want-or-not-container">
-
+        <input type="checkbox" id="switch" />
+        <label
+          for="switch"
+          onClick={() => {
+            if (want == true) {
+              setWant(false);
+            } else {
+              setWant(true);
+            }
+          }}
+        ></label>
+        <p>
+          {want ? "I want one out of these only" : "I do not want any of these"}
+        </p>
       </div>
     </div>
-  )
+  );
 };
 
 export default Lectures;
