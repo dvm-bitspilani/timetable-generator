a2a96c2c7cfff2e66c77542cd351934d357e96ea
Luv Gupta <lg5122003@gmail.com>
2023-06-17 23:18:31 +0530

    feat: add want/don't want overlay

--- src/components/JSX/CDCs.jsx
+++ src/components/JSX/CDCs.jsx
@@ -2,15 +2,68 @@ import React from "react";
 import IconBookWhite from "../../assets/IconBookWhite.svg";
 import "../CSS/CDCs.css";
 
-const CDCs = ({ onCourseClick , fetchedArray , courseIsSelectedGreen}) => {
-  const courseClickUnique =(id)=>{
+const CDCs = ({ onCourseClick, fetchedArray, courseIsSelectedGreen }) => {
+  const courseClickUnique = (id) => {
     onCourseClick(id);
+  };
+
+  const wantedSections = JSON.parse(localStorage.getItem("wantedSections"));
+  const unWantedSections = JSON.parse(localStorage.getItem("unwantedSections"));
+
+  const getWantedSection = (title) => {
+    const formattedTitle = title.replace(/\s/g, "");
+    const courseRegex = new RegExp(`^..+-${formattedTitle}$`);
+    let wantedArray = wantedSections.filter((section) =>
+      courseRegex.test(section)
+    );
+    let formattedArray = wantedArray
+      .map((section) => section.slice(0, 2))
+      .join(", ");
+    return formattedArray;
+  };
+
+  const getUnWantedSection = (title) => {
+    const formattedTitle = title.replace(/\s/g, "");
+    const courseRegex = new RegExp(`^..+-${formattedTitle}$`);
+    let wantedArray = unWantedSections.filter((section) =>
+      courseRegex.test(section)
+    );
+    let formattedArray = wantedArray
+      .map((section) => section.slice(0, 2))
+      .join(", "); //Check for 2 digit sections
+    return formattedArray;
   };
 
   return (
     <div className="courses-container">
       {fetchedArray?.cdcs?.map((item) => (
-        <div key={item.course_id} className={`course-div ${courseIsSelectedGreen(item.course_title) ? "courseIsSelectedGreen" : ""}`} onClick={() => courseClickUnique(item.course_title)}>
+        <div
+          key={item.course_id}
+          className={`course-div ${
+            courseIsSelectedGreen(item.course_title)
+              ? "courseIsSelectedGreen"
+              : ""
+          }`}
+          onClick={() => courseClickUnique(item.course_title)}
+        >
+          <div className="course-div-overlay">
+            <div className="course-overlay-content">
+              {getWantedSection(item.course_title) != "" ? (
+                <div className="want-content">
+                  Want: {getWantedSection(item.course_title)}
+                </div>
+              ) : (
+                ""
+              )}
+              {getUnWantedSection(item.course_title) != "" ? (
+                <div className="want-content">
+                  Don't Want: {getUnWantedSection(item.course_title)}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
           <img src={IconBookWhite} alt="book" />
           <h3>{item.course_title}</h3>
         </div>
--- src/components/JSX/MoreCourses.jsx
+++ src/components/JSX/MoreCourses.jsx
@@ -1,24 +1,76 @@
-import React , {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import IconBook from "../../assets/IconBook.svg";
 import IconBookWhite from "../../assets/IconBookWhite.svg";
 import "../CSS/CDCs.css";
 
-const MoreCourses = ({onCourseClick,courseIsSelectedGreen}) => {
+const MoreCourses = ({ onCourseClick, courseIsSelectedGreen }) => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    const storedCourses = JSON.parse(localStorage.getItem("storedMoreCourses")) || [];
+    const storedCourses =
+      JSON.parse(localStorage.getItem("storedMoreCourses")) || [];
     setCourses(storedCourses);
   }, []);
 
-  const courseClickUnique =(id)=>{
+  const courseClickUnique = (id) => {
     onCourseClick(id);
+  };
+
+  const wantedSections = JSON.parse(localStorage.getItem("wantedSections"));
+  const unWantedSections = JSON.parse(localStorage.getItem("unwantedSections"));
+
+  const getWantedSection = (title) => {
+    const formattedTitle = title.replace(/\s/g, "");
+    const courseRegex = new RegExp(`^..+-${formattedTitle}$`);
+    let wantedArray = wantedSections.filter((section) =>
+      courseRegex.test(section)
+    );
+    let formattedArray = wantedArray
+      .map((section) => section.slice(0, 2))
+      .join(", ");
+    return formattedArray;
+  };
+
+  const getUnWantedSection = (title) => {
+    const formattedTitle = title.replace(/\s/g, "");
+    const courseRegex = new RegExp(`^..+-${formattedTitle}$`);
+    let wantedArray = unWantedSections.filter((section) =>
+      courseRegex.test(section)
+    );
+    let formattedArray = wantedArray
+      .map((section) => section.slice(0, 2))
+      .join(", "); //Check for 2 digit sections
+    return formattedArray;
   };
 
   return (
     <div className="courses-container">
       {courses.map((item) => (
-        <div key={item.id} className={`course-div ${courseIsSelectedGreen(item.title) ? "courseIsSelectedGreen" : ""}`} onClick={()=> courseClickUnique(item.title)}>
+        <div
+          key={item.id}
+          className={`course-div ${
+            courseIsSelectedGreen(item.title) ? "courseIsSelectedGreen" : ""
+          }`}
+          onClick={() => courseClickUnique(item.title)}
+        >
+          <div className="course-div-overlay">
+            <div className="course-overlay-content">
+              {getWantedSection(item.title) != "" ? (
+                <div className="want-content">
+                  Want: {getWantedSection(item.title)}
+                </div>
+              ) : (
+                ""
+              )}
+              {getUnWantedSection(item.title) != "" ? (
+                <div className="want-content">
+                  Don't Want: {getUnWantedSection(item.title)}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
           <img src={IconBookWhite} alt="book" />
           <h3>{item.name}</h3>
         </div>
