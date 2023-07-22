a20b31129eb103bfcb44752bc4a21803cb7851a4
Luv Gupta <lg5122003@gmail.com>
2023-07-22 17:30:23 +0530

    style: responsive fix

--- src/components/JSX/DevTeam.jsx
+++ src/components/JSX/DevTeam.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CrossIcon from "../../assets/IconCross.svg";
+import BackIcon from "../../assets/IconLeftArrow.svg";
 const DevTeam = ({ showTeam, currentTeam, teamArray, showDev }) => {
   return (
     <>
@@ -11,18 +12,24 @@ const DevTeam = ({ showTeam, currentTeam, teamArray, showDev }) => {
             src={CrossIcon}
             alt=""
             className="dev-cross-btn"
-            onClick={showTeam}
+            onClick={showDev}
           />
           <div className="dev-teams-wrapper">
             <div className="dev-teams">
               {teamArray.map((item) => (
                 <div key={item.id} className="team">
                   <div className="team-logo">
-                      <img src={item.logo} alt="Team" />
+                    <img src={item.logo} alt="Team" />
                   </div>
                   <p>{item.name}</p>
                 </div>
               ))}
+            </div>
+          </div>
+          <div className="team-back">
+            <div className="team-back-btn" onClick={showTeam}>
+              <img src={BackIcon} alt="" />
+              <p>Back</p>
             </div>
           </div>
         </div>
